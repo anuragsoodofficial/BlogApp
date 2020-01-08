ee819f87566b4510398f8dfe0508baa2cd45258c
quincus <anurag.sood007@gmail.com>
2020-01-08 17:41:47 +0530

    Commenter will be autofilled

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,5 @@
 class CommentsController < ApplicationController
 
-  http_basic_authenticate_with name: "admin", password: "12345", only: :destroy
-
 	def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
