ee43492395d500577d7e346bc43b9dcc748ecec3
anuragsoodofficial <anurag.sood007@gmail.com>
2020-02-04 17:07:28 +0530

    validation error messages on frontend

---
+++ app/helpers/form_helper.rb
@@ -0,0 +1,15 @@
+module FormHelper
+  def errors_for(form, field)
+    content_tag(:p, form.object.errors[field].try(:first), class: 'help-block', id: "alert")
+  end
+
+  def form_group_for(form, field, opts={}, &block)
+    label = opts.fetch(:label) { true }
+    has_errors = form.object.errors[field].present?
+
+    content_tag :div, class: "form-group #{'has-error' if has_errors}" do
+      concat capture(&block)
+      concat errors_for(form, field)
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,6 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
   validates :password, format: Password_Requirement
+
+
 end
