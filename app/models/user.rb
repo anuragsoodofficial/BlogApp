71f6ce3537e137a3dd46699139b56c348d62ab3e
anuragsoodofficial <anurag.sood007@gmail.com>
2020-02-04 12:45:42 +0530

    Add password strength and error message

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,18 @@
 class User < ApplicationRecord
   has_secure_password
 
+  Password_Requirement = /\A
+  (?=.{8,}) #Atleast 8 characters long
+  (?=.*\d) #Atleast contain 1 number
+  (?=.*[a-z]) #Contain atleast one lowercase letter
+  (?=.*[A-Z]) #Contain atleast one uppercase letter
+  (?=.*[[:^alnum:]]) #Contain atleast one symbol
+  /x
+
   enum role: { admin: 0, user: 1 }
 
   has_many :articles, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
+  validates :password, format: Password_Requirement
 end
